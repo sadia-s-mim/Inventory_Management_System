98b32eecd3ec032770b9ab1a788b2ea702fb10f1
Mahmuda Akter Rina <ayesha09nur@gmail.com>
2026-08-17 00:03:32 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+</div><!-- /.pc-wrapper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/app.js"></script>
+</body>
+</html>
